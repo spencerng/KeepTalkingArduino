61864f6f56555adaa4e986b2cb0d19f568ed1198
Dor Rondel <dor.rondel@gmail.com>
2016-09-10 17:04:50 -0400

    solved rgb issue

--- SimonSays.ino
+++ SimonSays.ino
@@ -8,18 +8,17 @@
 #define YELLOW 4
 
 
-
 void flashColor(int color){
   
   if(color==GREEN){
-    analogWrite(4,HIGH);
+    digitalWrite(A4,HIGH);
     delay(1500);
-    analogWrite(4,LOW);
+    digitalWrite(A4,LOW);
   }
   if(color==RED){
-    analogWrite(5,HIGH);
+    digitalWrite(A5,HIGH);
     delay(1500);
-    analogWrite(5,LOW);
+    digitalWrite(A5,LOW);
   }
   if(color==YELLOW){
     digitalWrite(8,255);
@@ -38,36 +37,40 @@ void flashColor(int color){
 
 }
 
+int numberPassed = 3;
+
 void flashColors(int seq[]){
-  int size = sizeof(seq)/sizeof(int);
-  for(int i = 0; i<size;i++){
+  int size2 = numberPassed+1;
+  for(int i = 0; i<size2;i++){
     flashColor(seq[i]);
-    delay(750);
+    delay(250);
   }
-    
+  Serial.println("Iteration ended"); 
   delay(2000);
 }
 
-int numberPassed = 3;
+
 
 
 void setup() {
   pinMode(11,OUTPUT);
   pinMode(7,OUTPUT);
   pinMode(8,OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
   
-  randomSeed(3);
+  randomSeed(analogRead(0));
   int colors[4];
   for(int i = 0; i < 4; i++)
-    colors[i] = random() % 4;
+    colors[i] = random(4);
   
   int alwaysOnPins[] = {1,3,5,9,12};
   for(int i = 0; i < 5; i++){
     pinMode(alwaysOnPins[i], OUTPUT);
     digitalWrite(alwaysOnPins[i], HIGH);
   }
-  analogWrite(3,HIGH);
-
+  digitalWrite(A3,HIGH);
+  
   
   
   while(numberPassed<4){
@@ -83,6 +86,5 @@ void setup() {
 
 
 void loop() {
-  
-  
+
 }
